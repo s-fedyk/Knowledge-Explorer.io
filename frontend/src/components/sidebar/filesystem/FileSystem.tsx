3e9d1b3e96a4b30da403554b8db960bfb37f7c5a
sfedyk <stan.fedyk@truenorthmortgage.ca>
2025-05-15 08:55:09 -0600

    border

--- frontend/src/components/sidebar/filesystem/FileSystem.tsx
+++ frontend/src/components/sidebar/filesystem/FileSystem.tsx
@@ -5,7 +5,7 @@ import FolderView from "./FolderView";
 function FileSystem() {
   return (
     <div className="h-full">
-      <div className="flex justify-center items-center p-2 border-b border-gray-400">
+      <div className="flex justify-center items-center p-2 border-b border-t border-gray-400">
         <h2 className="p-2 text-black text-lg font-bold">Knowledge Base</h2>
       </div>
       <FolderView />
