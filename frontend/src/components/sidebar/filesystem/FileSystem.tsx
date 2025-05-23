83d1c5336fe14965050144909729dcb129f80f21
sfedyk <stan.fedyk@truenorthmortgage.ca>
2025-05-23 00:21:39 -0600

    brain

--- frontend/src/components/sidebar/filesystem/FileSystem.tsx
+++ frontend/src/components/sidebar/filesystem/FileSystem.tsx
@@ -6,7 +6,7 @@ function FileSystem() {
   return (
     <div className="h-full">
       <div className="flex justify-center items-center p-2 border-b border-t border-gray-400">
-        <h2 className="p-2 text-black text-lg font-bold">Knowledge Base</h2>
+        <h2 className="p-2 text-black text-lg font-bold">Knowledge🧠Base</h2>
       </div>
       <FolderView />
     </div>
